77bbb57e8afaf6bf5c345497067cf1def66361e9
NingXingX <1248502068@qq.com>
2024-01-12 02:51:45 +0800

    修改表格错误

--- Assets/Scripts/GamePlay/GamePlayAction/SpawnRoleAction.cs
+++ Assets/Scripts/GamePlay/GamePlayAction/SpawnRoleAction.cs
@@ -14,7 +14,7 @@ public class SpawnRoleAction : FsmStateAction
         var board = BoardMapCtrl.Instance;
 
         //生成所需要的角色到棋盘上,输入对应的特性
-        this.DebugSpawnRole(10003, 1, 1, 1, 1, new List<int> { 10017, 10012 }, new List<int> { 1000101 });//游侠
+        this.DebugSpawnRole(10003, 1, 1, 1, 1, new List<int> { 10017, 10012 }, new List<int> { 1000301,1000302 });//游侠
         this.DebugSpawnRole(10004, 1, 1, 0, 2, new List<int> { 10011}, new List<int> { 1000101 });//剑士
         this.DebugSpawnRole(10005, 1, 2, 5, 3, new List<int> { 10012}, new List<int> { 1000101 });//铁甲兵（敌人test）
 
